2640d19c52d88827e9b900734b21d10446d2f4e1
Umar A. <55955944+Croosh@users.noreply.github.com>
2023-12-09 16:00:06 +0500

    Navigation

--- src/Cheatsheet/Cheatsheet.tsx
+++ src/Cheatsheet/Cheatsheet.tsx
@@ -1,16 +1,22 @@
 import { Button } from "@/components/ui/button";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Cheatsheet() {
+  const navigate = useNavigate();
   return (
     <>
       <div className="flex h-screen justify-center items-center font-bold font-['Inter'] text-6xl">
         <div>
           <h1>Comming Soon.</h1>
           <div className="h-5"></div>
-          <Link to={"http://localhost:5173"}>
-            <Button className="self-center">Go Back</Button>
-          </Link>
+          <Button
+            className="self-center"
+            onClick={() => {
+              navigate(-1);
+            }}
+          >
+            Go Back
+          </Button>
         </div>
       </div>
     </>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,28 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Button } from "./ui/button";
 
 function Header(props) {
+  const navigate = useNavigate();
   return (
     <>
       {props.isGenerator ? (
         <>
           <header className=" w-screen flex justify-between  py-6 px-16 items-center">
-            <Link to={"http://localhost:5173/"}>
-              <div className="text-xl font-bold font-['Ubuntu_Mono'] text-slate-950/80 antialiased">
-                #.mdfiles
-              </div>
-            </Link>
-            <Link to={"http://localhost:5173/cheatsheet"}>
-              <Button
-                className=" font-['Inter'] font-semibold text-md bg-none"
-                variant={"ghost"}
-              >
-                Cheatsheet
-              </Button>
-            </Link>
+            <Button
+              variant={"ghost"}
+              className="text-xl font-bold font-['Ubuntu_Mono'] text-slate-950/80 antialiased hover:bg-slate-400/20"
+            >
+              #.mdfiles
+            </Button>
+            <Button
+              onClick={() => {
+                navigate(-1);
+              }}
+              className=" font-['Inter'] font-semibold text-md bg-none"
+              variant={"ghost"}
+            >
+              Cheatsheet
+            </Button>
           </header>
         </>
       ) : null}
@@ -27,11 +30,12 @@ function Header(props) {
       {props.isHomepage ? (
         <>
           <header className=" w-screen flex justify-between  py-6 px-16 items-center absolute">
-            <Link to={"http://localhost:5173/"}>
-              <div className="text-xl font-bold font-['Ubuntu_Mono'] text-slate-950/80 antialiased">
-                #.mdfiles
-              </div>
-            </Link>
+            <Button
+              variant={"ghost"}
+              className="text-xl font-bold font-['Ubuntu_Mono'] text-slate-950/80 antialiased hover:bg-slate-400/20"
+            >
+              #.mdfiles
+            </Button>
           </header>
         </>
       ) : null}
