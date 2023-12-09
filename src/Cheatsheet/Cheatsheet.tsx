55a1649e46cd862334987a6829f896a85de193a1
Umar A. <55955944+Croosh@users.noreply.github.com>
2023-12-09 16:35:11 +0500

    Responsive Cheatsheet

--- src/Cheatsheet/Cheatsheet.tsx
+++ src/Cheatsheet/Cheatsheet.tsx
@@ -5,9 +5,11 @@ export default function Cheatsheet() {
   const navigate = useNavigate();
   return (
     <>
-      <div className="flex h-screen justify-center items-center font-bold font-['Inter'] text-6xl">
+      <div className="flex h-screen justify-center items-center">
         <div className="flex justify-center items-center flex-col text-center">
-          <h1>Comming Soon.</h1>
+          <h1 className=" font-bold font-['Inter'] text-6xl max-md:text-3xl">
+            Comming Soon.
+          </h1>
           <div className="h-5"></div>
           <Button
             onClick={() => {
